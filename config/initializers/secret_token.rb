71c34d40fc98845d50acd70d53119a3c026d78bc
Anil Punjabi <anilo@hotmail.com>
2011-12-03 23:32:11 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tallydev::Application.config.secret_token = 'db482f2ed4db88beb04c6d2f15bc31518cca949d3df73fe304d44d3ecec6c32f9e13e85d36c4306cc2cb7f1297da970ad4a3ca9e3221ea8647f59f0bee7be8be'
